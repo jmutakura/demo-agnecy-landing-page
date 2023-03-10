a148fb76a404245754a4248b03197a4f31b8733b
Jonathan Mutakura <jonathan.mutakura@pivotfurniture.com>
2023-03-09 18:18:09 -0600

    Added the pricing section and revamped the components directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import HeroSection from '@/components/HeroSection';
-import Features from '@/components/Features';
-import Testimonials from '@/components/Testimonials';
+import HeroSection from '@/components/Page/HeroSection';
+import Features from '@/components/Page/Features';
+import Testimonials from '@/components/Page/Testimonials';
+import Pricing from '@/components/Page/Pricing';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,7 @@ export default function Home() {
 			<HeroSection />
 			<Features />
 			<Testimonials />
+			<Pricing />
 		</>
 	);
 }
